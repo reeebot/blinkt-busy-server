c28c8c029a138413ee3e9c351fab83153962c447
Tim Meijome <reeebot@gmail.com>
2020-05-12 09:52:40 -0700

    major change
    update unicorn hat to blinkt hat.  removed POST setcolor endpoint and added busy, away, and available API endpoints instead.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-import unicornhat as unicorn
+import blinkt
 import threading
 from time import sleep
 from datetime import datetime
@@ -11,113 +11,64 @@ from flask import Flask, jsonify, make_response, request
 from random import randint
 
 blinkThread = None
-globalRed = 0
-globalGreen = 0
-globalBlue = 0
-globalLastCalled = None
-globalLastCalledApi = None
 
-#setup the unicorn hat
-unicorn.set_layout(unicorn.AUTO)
-unicorn.brightness(0.5)
+#setup the blinkt! hat
+blinkt.set_brightness(0.2)
+blinkt.show()
 
-#get the width and height of the hardware
-width, height = unicorn.get_shape()
 
 app = Flask(__name__)
 
-def setColor(r, g, b, brightness, speed) :
-	global crntColors, globalBlue, globalGreen, globalRed
-	globalRed = r
-	globalGreen = g
-	globalBlue = b
 
-	if brightness != '' :
-		unicorn.brightness(brightness)
-
-	for y in range(height):
-		for x in range(width):
-			unicorn.set_pixel(x, y, r, g, b)
-	unicorn.show()
-
-	if speed != '' :
-		sleep(speed)
-		unicorn.clear()
-		crntT = threading.currentThread()
-		while getattr(crntT, "do_run", True) :
-			for y in range(height):
-				for x in range(width):
-					unicorn.set_pixel(x, y, r, g, b)
-			unicorn.show()
-			sleep(speed)
-			unicorn.clear()
-			unicorn.show()
-			sleep(speed)
-		
 def switchOn() :
 	red = randint(10, 255)
 	green = randint(10, 255)
 	blue = randint(10, 255)
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, '', ''))
+	blinkThread = threading.Thread(target=setColor, args=(red, green, blue))
 	blinkThread.do_run = True
 	blinkThread.start()
 
 def switchOff() :
-	global blinkThread, globalBlue, globalGreen, globalRed
-	globalRed = 0
-	globalGreen = 0
-	globalBlue = 0
 	if blinkThread != None :
 		blinkThread.do_run = False
-	unicorn.clear()
-	unicorn.off()
-
-def setTimestamp() :
-	global globalLastCalled
-	globalLastCalled = datetime.now()
+	blinkt.clear()
+	blinkt.show()
 
 # API Initialization
 @app.route('/api/on', methods=['GET'])
 def apiOn() :
-	global globalLastCalledApi
-	globalLastCalledApi = '/api/on'
 	switchOff()
 	switchOn()
-	setTimestamp()
 	return jsonify({})
 
 @app.route('/api/off', methods=['GET'])
 def apiOff() :
-	global crntColors, globalLastCalledApi
-	globalLastCalledApi = '/api/off'
-	crntColors = None
 	switchOff()
-	setTimestamp()
 	return jsonify({})
 
-@app.route('/api/switch', methods=['POST'])
-def apiSwitch() :
-	global blinkThread, globalLastCalledApi
-	globalLastCalledApi = '/api/switch'
+@app.route('/api/busy', methods=['GET'])
+def apiBusy() :
 	switchOff()
-	content = request.json
-	red = content.get('red', '')
-	green = content.get('green', '')
-	blue = content.get('blue', '')
-	brightness = content.get('brightness', '')
-	speed = content.get('speed', '')
-	blinkThread = threading.Thread(target=setColor, args=(red, green, blue, brightness, speed))
+	blinkThread = threading.Thread(target=setColor, args=(255, 0, 0))
 	blinkThread.do_run = True
 	blinkThread.start()
-	setTimestamp()
-	return make_response(jsonify())
+	return (jsonify())
 
-@app.route('/api/status', methods=['GET'])
-def apiStatus() :
-	global globalBlue, globalGreen, globalRed, globalLastCalled, globalLastCalledApi
-	cpu = CPUTemperature()
-	return jsonify({ 'red': globalRed, 'green': globalGreen, 'blue': globalBlue, 'lastCalled': globalLastCalled, 'cpuTemp': cpu.temperature, 'lastCalledApi': globalLastCalledApi })
+@app.route('/api/available', methods=['GET'])
+def apiBusy() :
+	switchOff()
+	blinkThread = threading.Thread(target=setColor, args=(0, 255, 0))
+	blinkThread.do_run = True
+	blinkThread.start()
+	return (jsonify())
 
+@app.route('/api/away', methods=['GET'])
+def apiBusy() :
+	switchOff()
+	blinkThread = threading.Thread(target=setColor, args=(150, 150, 0))
+	blinkThread.do_run = True
+	blinkThread.start()
+	return (jsonify())
 
 @app.errorhandler(404)
 def not_found(error):
